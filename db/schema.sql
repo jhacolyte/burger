5df18fa2336c04709ba42b512f14d7bf16d96700
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-09-18 02:31:27 -0700

    database added

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+
+-- create database named burgers_db
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table named burgers
+CREATE TABLE burgers
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+burger_name varchar
+    (255) NOT NULL,
+devoured boolean not null default 0, 
+PRIMARY KEY
+    (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Insert a set of records.
+INSERT INTO burgers_db.burgers
+    (burger_name, devoured)
+VALUES
+    ('Lasagna Burger', false),
+    ('Broccoli Burger', false),
+    ('Orang Banana Burger', false);
