db882bc27e0a953986bf4e6642d15a139bde9ab6
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-09-19 02:22:16 -0700

    contorllers is done moving on to html files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,4 +16,25 @@ router.get("/burgers", function(req, res) {
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
+
+  router.post("/burgers/create", function(req, res) {
+    burgers.insertOne(["burger_name"], [req.body.burger_name], function(data) {
+      res.redirect("/burgers");
+    });
+  });
+
+  router.put("/burgers/update/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burgers.updateOne(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function(data) {
+        res.redirect("/burgers");
+      }
+    );
+  });
 });
