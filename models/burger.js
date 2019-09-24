cf8c6ae352f1620e93c0d659dab89e1b56d7194a
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-09-24 04:15:56 -0700

    added some style in application

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,27 @@
 var orm = require("../config/orm.js");
 
 var burger = {
+  // selects all burgers in the database
   selectAll: function(cb) {
-    orm.selectAll(function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
-  insertOne: function(burger, cb) {
-    orm.insertOne(burger, function(res) {
+  // inserts one record into the burgers table
+  insertOne: function(burgerName, cb) {
+    orm.insertOne("burgers", "burger_name", burgerName, function(res) {
       cb(res);
     });
   },
-  updateOne: function(id, cb) {
-    orm.updateOne([id], function(res) {
+  // updates a burger entry
+  updateOne: function(idNum, cb) {
+    orm.updateOne("burgers", "devoured", "1", "id", idNum, function(res) {
+      cb(res);
+    });
+  },
+  // deletes a burger entry
+  deleteOne: function(idNum, cb) {
+    orm.deleteOne("burgers", "id", idNum, function(res) {
       cb(res);
     });
   }
