dc740e53933d09dfdf381d0988f1102f48bb9726
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-09-24 04:14:02 -0700

    rebuilding the js files to isolate the error

--- config/orm.js
+++ config/orm.js
@@ -1,33 +1,39 @@
-var connection = require("../config/connection.js");
+// imports the connection module
+var connection = require("./connection.js");
 
 var orm = {
-  selectAll: function(cb) {
-    var queryString = "SELECT * FROM burgers";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+  selectAll: function(table, cb) {
+    var query = "SELECT * FROM ??;";
+    connection.query(query, [table], function(err, result) {
+      if (err) throw err;
       cb(result);
     });
   },
-
-  insertOne: function(burger, cb) {
-    var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
-    connection.query(queryString, [burger], function(err, result) {
-      if (err) {
-        throw err;
-      }
+  insertOne: function(table, colName, colValue, cb) {
+    var query = "INSERT INTO ?? (??) VALUES (?);";
+    connection.query(query, [table, colName, colValue], function(err, result) {
+      if (err) throw err;
+      console.log("Successfully inserted: " + result);
       cb(result);
     });
   },
-
-  updateOne: function(id, cb) {
-    var queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
-
-    connection.query(queryString, [id], function(err, result) {
-      if (err) {
-        throw err;
+  updateOne: function(table, colName, colValue, idCol, idValue, cb) {
+    var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?;";
+    connection.query(
+      query,
+      [table, colName, colValue, idCol, idValue],
+      function(err, result) {
+        if (err) throw err;
+        console.log("Successfully updated: " + result);
+        cb(result);
       }
+    );
+  },
+  deleteOne: function(table, idCol, idValue, cb) {
+    var query = "DELETE FROM ?? WHERE ?? = ?;";
+    connection.query(query, [table, idCol, idValue], function(err, result) {
+      if (err) throw err;
+      console.log("Successfully deleted: " + result);
       cb(result);
     });
   }
